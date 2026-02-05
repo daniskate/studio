88a572262b05907d556777d1b28a03a1d55e44f7
Daniele Monari <dani.mona120@gmail.com>
2026-02-05 15:50:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-449202787-10d46",
+  "appId": "1:237322058146:web:906080efbeea0929df1afb",
+  "apiKey": "AIzaSyBXvkSnxp6LImtx-yyzRPlg_soni30Yrek",
+  "authDomain": "studio-449202787-10d46.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "237322058146"
+};
